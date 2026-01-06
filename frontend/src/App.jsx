8fd86df445dec8e0a832c141714fdf36cc0613ce
Sherzod <omilxanovs7@gmail.com>
2026-01-06 15:11:40 +0500

    Sittings Page & Theme select

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,9 +9,11 @@ import SettingsPage from './pages/SettingsPage'
 import SignUpPage from './pages/SignUpPage'
 import { useAuthStore } from './store/useAuthStore'
 import { Toaster } from 'react-hot-toast'
+import { useThemeStore } from './store/useThemeStore'
 function App() {
 	const { authUser, checkAuth, isCheckingAuth } = useAuthStore()
 
+	const {theme} = useThemeStore()
 	useEffect(() => {
 		checkAuth()
 	}, [authUser])
@@ -25,7 +27,7 @@ function App() {
 			</div>
 		)
 	return (
-		<div>
+		<div data-theme={theme}>
 			<Navbar />
 
 			<Routes>
